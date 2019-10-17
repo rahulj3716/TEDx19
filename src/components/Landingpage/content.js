68890101b79e34f90df33c3b4167e7707e635a3a
rahulj3716 <rahulj3716@gmail.com>
2019-10-17 19:16:55 +0530

    created speaker page

---
+++ src/components/Landingpage/content.js
@@ -0,0 +1,47 @@
+export const speakers = [
+  {
+    id: 1,
+    firstName: "Bill",
+    lastName: "Gates",
+    img: "./src/images/download.jpeg",
+    designation: "( Founder, Microsoft )",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+  },
+  {
+    id: 2,
+    firstName: "bill",
+    lastName: "gates",
+    img: "./src/images/download.jpeg",
+    designation: "( Founder, Microsoft )",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+  },
+  {
+    id: 3,
+    firstName: "bill",
+    lastName: "gates",
+    img: "./src/images/download.jpeg",
+    designation: "( Founder, Microsoft )",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+  },
+  {
+    id: 4,
+    firstName: "bill",
+    lastName: "gates",
+    img: "./src/images/download.jpeg",
+    designation: "( Founder, Microsoft )",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+  },
+  {
+    id: 5,
+    firstName: "Bill",
+    lastName: "gates",
+    img: "./src/images/download.jpeg",
+    designation: "( Founder, Microsoft )",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+  }
+];
