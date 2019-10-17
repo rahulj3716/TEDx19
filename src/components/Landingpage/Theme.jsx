a19de1c6f218d5bf2651e0c370f0bcb4207574a9
prakhar2109 <prakharagarwal21091998@gmail.com>
2019-10-17 12:51:18 +0530

    theme added

--- src/components/Landingpage/Theme.jsx
+++ src/components/Landingpage/Theme.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './src/css/theme.css'
-import { Carousel } from 'antd';
+import Heading from './Heading';
 export default class ThemeIndex extends Component {
     constructor(props) {
         super(props);
@@ -11,25 +11,16 @@ export default class ThemeIndex extends Component {
         return (
             <div>
                 <div className="landing-Theme-parent">
-                    <div className="landing-Theme-parent-heading">
-                        <div className="landing-Theme-parent-heading-line">
-                        </div>
-                        <div className="landing-Theme-parent-heading-content">
-                            Theme
-                        </div>
-
-                    </div>
 
                     <div className="landing-Theme-slider-parent">
                         <div className="landing-Theme-slider-parent-pic">
+                            <Heading text="THEME"/>
                         </div>
                         <div className="landing-Theme-slider-child">
-                        <Carousel effect="fade" className="Theme-slider">
-                            <div><h3>1</h3></div>
-                            <div><h3>2</h3></div>
-                            <div><h3>3</h3></div>
-                            <div><h3>4</h3></div>
-                        </Carousel>,
+                        Since the beginning of time, humans have been surrounded by constraints and challenged with the task to overcome them. A huge part of our evolution revolves around these constraints. Although culture and tradition have provided us with a basis for living standards, they have held us back in many ways. Going against the tradition is not to be viewed as a step in the wrong direction, but rather as an attempt at a new, unexplored adventure.
+                        <br/><br/>
+                        This year, TEDxIITRoorkee is focused on encouraging people to go against the flow and try out new ventures, and also guide them along their journey by conducting this year’s talks on the theme "Beyond the Barriers". Our aim is to bring together a diverse group of speakers who can share their thoughts, insights, and experiences and enlighten the audience by their presence.
+
                         </div>
                         
                     </div>
