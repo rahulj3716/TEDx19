43362245b6f67bdbce34a333174120e8a31db95d
Prateek Tripathi <ptripathi@mt.iitr.ac.in>
2019-10-19 01:04:31 +0530

    Team page

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar'
-
+import './src/css/index.css'
+import '../../index.css'
+import Member from './Member'
 
 export default class TeamIndex extends Component {
     constructor(props) {
@@ -12,7 +14,20 @@ export default class TeamIndex extends Component {
         return (
             <div style={{backgroundImage: "linear-gradient(to right, #1e1e26, #291e24 57%, #341f21)"}}>
                 <section id="landing-page-navbar"> <Navbar /></section>
-                hello
+                <div className="landing-page">
+                    <div className="landing-page-parent">
+                        <h1 className="landing-page-heading">TEAM</h1>
+                        <div className="landing-page-image"></div>
+                    </div>
+                    <ul className="member-list">
+                        <Member className="member-list-child"/>
+                        <Member className="member-list-child"/>
+                        <Member className="member-list-child"/>
+                        <Member className="member-list-child"/>
+                        <Member className="member-list-child"/>
+                        <Member className="member-list-child"/>
+                    </ul>
+                </div>
             </div>
 
         )
